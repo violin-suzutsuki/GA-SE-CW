7389f32dacf48e634f3d8f0f29332858d2b74975
violin-suzutsuki <93684340+violin-suzutsuki@users.noreply.github.com>
2022-03-10 22:41:41 +0000

    refactor, javadoc tomfoolery

--- src/main/java/com/SET08103/cw/api/Controller.java
+++ src/main/java/com/SET08103/cw/api/Controller.java
@@ -21,26 +21,43 @@ import java.util.List;
  */
 @RestController
 public class Controller {
+    /**
+     * api endpoint to handle the generation of reports and return the data in a json string
+     *
+     * @param id report id to run
+     * @param input any user input, defaults to "" if there is none
+     * @return a json string
+     */
     @RequestMapping("/api")
-    public String api(@RequestParam(value="reportId") Integer id) throws JsonProcessingException {
+    public String api(@RequestParam(value="reportId") Integer id, @RequestParam(value="userInput", defaultValue="") String input) {
         DataHandler dataHandler = DataHandler.getInstance();
-        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
-
-        String jsonRet = "";
 
         switch (id)
         {
             // https://github.com/violin-suzutsuki/GA-SE-CW/issues/22
             // Create Report: All the countries in the world organised by largest population to smallest
             case 22:
-                List<Country> countries = DataParser.getCountriesInContinents(dataHandler.getContinents());
+            {
+                List<Country> countries = DataParser.getCountriesInWorld();
                 countries.sort(Comparator.comparing(Country::getPopulation).reversed());
 
-                jsonRet = objectWriter.writeValueAsString(countries);
+               return DataParser.toJson(countries);
+            }
 
-                break;
+            // https://github.com/violin-suzutsuki/GA-SE-CW/issues/23
+            // Create Report: All the countries in a continent organised by largest population to smallest
+            case 23:
+            {
+                List<Country> countries = DataParser.getCountriesInContinent(input);
+                countries.sort(Comparator.comparing(Country::getPopulation).reversed());
+
+                return DataParser.toJson(countries);
+            }
+
+            default:
+            {
+                return "";
+            }
         }
-
-        return jsonRet;
     }
 }
--- src/main/java/com/SET08103/cw/data/DataParser.java
+++ src/main/java/com/SET08103/cw/data/DataParser.java
@@ -3,9 +3,12 @@ package com.SET08103.cw.data;
 import com.SET08103.cw.objects.Continent;
 import com.SET08103.cw.objects.Country;
 import com.SET08103.cw.objects.Region;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * DataHandler.java
@@ -13,13 +16,73 @@ import java.util.List;
  * This is the class that handles parsing the in-memory sql objects. It is responsible for generating the data used for reports.
  */
 public final class DataParser {
-    public static List<Country> getCountriesInContinents(List<Continent> continents)
+    /**
+     * Parses any java object into a json string.
+     *
+     * @param object the object to serialize
+     * @return a json string
+     */
+    public static String toJson(Object object)
     {
+        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+        String json;
+
+        try {
+            json = objectWriter.writeValueAsString(object);
+        }
+        catch(Exception e)
+        {
+            return "";
+        }
+
+        return json;
+    }
+
+    /**
+     * DataHandler wrapper to get all of the world's continents
+     *
+     * @return a list of continents.
+     */
+    public static List<Continent> getContinents()
+    {
+        return DataHandler.getInstance().getContinents();
+    }
+
+    /**
+     * Get all of the countries in the world sorted by population in descending order
+     *
+     * @return a list of countries
+     */
+    public static List<Country> getCountriesInWorld()
+    {
+        List<Continent> continents = getContinents();
         List<Country> countries = new ArrayList<Country>();
 
         for (Continent continent : continents) {
             for (Region region : continent.getRegions()) {
                 countries.addAll(region.getCountries());
+            }
+        }
+
+        return countries;
+    }
+
+    /**
+     * Get all of the countries in a continent sorted by population in descending order
+     *
+     * @param continentName continent name to search for, non caps-sensitive
+     * @return a list of countries
+     */
+    public static List<Country> getCountriesInContinent(String continentName)
+    {
+        List<Country> countries = new ArrayList<Country>();
+
+        for (Country country : getCountriesInWorld())
+        {
+            if (country.getContinent().toLowerCase().contains(continentName.toLowerCase()))
+            {
+                countries.add(country);
             }
         }
 
