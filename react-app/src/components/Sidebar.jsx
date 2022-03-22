d26a35f399b16f7a90a10b7443dd365454f5c7ed
Matthew Jack <mtjack@outlook.com>
2022-03-22 00:44:27 +0000

    data pulling through to table component

---
+++ react-app/src/components/Sidebar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState } from "react";
+
+function Sidebar({ setTableData }) {
+  // setTableData is from the App
+  const getData = (id) => {
+    const responseFromAPI = fetch(`/api/report?reportId=${id}`)
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        // do stuff with responseJSON here...
+        console.log(responseJSON);
+        setTableData(responseJSON);
+      });
+  };
+
+  const hideShowInput = () => {
+    console.log(`hiding showing input`);
+    // const userInput = document.getElementById("userInput");
+    // userInput.classlist.add();
+  };
+
+  const [urlId, setUrlId] = useState();
+
+  // const getValueFromSelect = (e) => {
+  //   const selectValue = e.target.value;
+  //   console.log(selectValue);
+  // };
+
+  return (
+    <>
+      SIDEBAR
+      <button onClick={() => getData(urlId)}>Generate</button>
+      <select onChange={(e) => setUrlId(e.target.value)}>
+        <option value="22">22</option>
+        <option value="23">23</option>
+        <option value="24">24</option>
+      </select>
+      <input id="userInput" type="text" />
+    </>
+  );
+}
+
+export default Sidebar;
