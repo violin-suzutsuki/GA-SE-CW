7c769713dbf5a85865f6ee947299fd44ec1286ca
Matthew Jack <matthew.jack@sds.co.uk>
2022-04-02 01:34:41 +0100

    table setup for country report

--- react-app/src/components/Table.jsx
+++ react-app/src/components/Table.jsx
@@ -1,15 +1,15 @@
 export default function Table({ tableData, reportId, isLoading }) {
 
-  if(isLoading == true) {
+  if (isLoading == true) {
     return (<p>Loading...</p>)
   }
 
   return (
     <div>
       {(() => {
-
         switch (reportId) {
           case '22':
+          case '23':
             return (
               <table>
                 <tr>
@@ -21,45 +21,44 @@ export default function Table({ tableData, reportId, isLoading }) {
                   <th>Capital</th>
                 </tr>
                 {tableData.map(data => (
-                  
                   <tr>
                     <td>{data.code}</td>
                     <td>{data.name}</td>
                     <td>{data.continent}</td>
                     <td>{data.region}</td>
                     <td>{data.population}</td>
-                    <td>{data.capital.name}</td>
+                    <td>{(data.capital == null) ? "No Data" : data.capital.name}</td>
                   </tr>
                 ))}
               </table>
             );
 
-            case '23':
-              return (
-                <table>
+          case '24':
+            return (
+              <table>
+                <tr>
+                  <th>2</th>
+                  <th>4</th>
+                  <th>6</th>
+                  <th>8</th>
+                </tr>
+                {tableData.map(data => (
                   <tr>
-                    <th>2</th>
-                    <th>4</th>
-                    <th>6</th>
-                    <th>8</th>
+                    <td>{data.code}</td>
+                    <td>{data.code}</td>
+                    <td>{data.code}</td>
+                    <td>{data.code}</td>
                   </tr>
-                  {tableData.map(data => (
-                    <tr>
-                      <td>{data.code}</td>
-                      <td>{data.code}</td>
-                      <td>{data.code}</td>
-                      <td>{data.code}</td>
-                    </tr>
-                  ))}
-                </table>
-              );
-        
+                ))}
+              </table>
+            );
+
           default:
-            return(
+            return (
               <p>Please select a report from the dropdown.</p>
             );
         }
       })()}
-  </div>
+    </div>
   );
 }
