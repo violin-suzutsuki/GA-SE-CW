6efcdc5797eab552c37a26bc6e116c5834518f9b
Matthew Jack <matthew.jack@sds.co.uk>
2022-04-05 13:33:04 +0100

    setup capital report with test data

--- react-app/src/components/Table.jsx
+++ react-app/src/components/Table.jsx
@@ -9,7 +9,6 @@ export default function Table({ tableData, reportId, isLoading }) {
       {(() => {
         switch (reportId) {
           case '22':
-          case '23':
           case '25':
             return (
               <table>
@@ -57,6 +56,27 @@ export default function Table({ tableData, reportId, isLoading }) {
                 )}
               </table>
             );
+
+          case '23': 
+            return (
+              <table>
+                <tr>
+                  <th>Name</th>
+                  <th>Country</th>
+                  <th>Population</th>
+                </tr>
+                {tableData.map(data => 
+                  {if(data.capital !== null) {
+                    return(
+                    <tr>
+                      <td>{data.capital.name}</td>
+                      <td>{data.name}</td>
+                      <td>{data.capital.population}</td>
+                    </tr>)
+                  }}
+                )}
+              </table>
+            )
 
           default:
             return (
