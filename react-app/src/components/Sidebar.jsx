b13e89f7319ed46530d777fac2e048d6f0f5f1a8
Matthew Jack <matthew.jack@sds.co.uk>
2022-04-11 15:26:16 +0100

    user input working via text box

--- react-app/src/components/Sidebar.jsx
+++ react-app/src/components/Sidebar.jsx
@@ -5,10 +5,10 @@ function Sidebar({ setTableData, setTableTpl, setLoading }) {
   const [inputs, setInputs] = useState({id: "", userInput: ""}) 
 
   // setTableData is from the App
-  const getData = (id) => {
+  const getData = (id, userInput) => {
     setTableTpl(id)
     setLoading(true)
-    const responseFromAPI = fetch(`/api/report?reportId=${id}`)
+    const responseFromAPI = fetch(`/api/report?reportId=${id}&userInput=${userInput}`)
       .then((response) => response.json())
       .then((responseJSON) => {
         // do stuff with responseJSON here...
@@ -16,7 +16,7 @@ function Sidebar({ setTableData, setTableTpl, setLoading }) {
         setTableData(responseJSON);
         
       });
-      setLoading(false);
+    setLoading(false);
   };
 
   const hideShowInput = () => {
@@ -28,13 +28,13 @@ function Sidebar({ setTableData, setTableTpl, setLoading }) {
 
   return (
     <>
-      <button onClick={() => getData(inputs.id)}>Generate</button>
+      <button onClick={() => getData(inputs.id, inputs.userInput)}>Generate</button>
       <select onChange={(e) => setInputs(values => ({...values, id: e.target.value}))}>
         <option value="22">22</option>
         <option value="23">23</option>
         <option value="24">24</option>
       </select>
-      <input id="userInput" type="text" />
+      <input onChange={(e) => setInputs(values => ({...values, userInput: e.target.value}))} type="text" />
     </>
   );
 }
