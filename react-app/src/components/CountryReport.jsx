180bf2e3651e47ab55ffbc767c7d640117917921
Matthew Jack <matthew.jack@sds.co.uk>
2022-04-20 10:00:03 +0100

    table styles added

--- react-app/src/components/CountryReport.jsx
+++ react-app/src/components/CountryReport.jsx
@@ -1,24 +1,28 @@
 export default function CountryReport({tableData}) {
     return (
-        <table>
-            <tr>
-                <th>Code</th>
-                <th>Name</th>
-                <th>Continent</th>
-                <th>Region</th>
-                <th>Population</th>
-                <th>Capital</th>
-            </tr>
-            {tableData.map(data => (
+        <table className="table table-zebra w-full">
+            <thead>
                 <tr>
-                <td>{data.code}</td>
-                <td>{data.name}</td>
-                <td>{data.continent}</td>
-                <td>{data.region}</td>
-                <td>{data.population}</td>
-                <td>{(data.capital == null) ? "-" : data.capital.name}</td>
+                    <th>Code</th>
+                    <th>Name</th>
+                    <th>Continent</th>
+                    <th>Region</th>
+                    <th>Population</th>
+                    <th>Capital</th>
                 </tr>
-            ))}
+            </thead>
+            <tbody>
+                {tableData.map(data => (
+                    <tr>
+                    <td>{data.code}</td>
+                    <td>{data.name}</td>
+                    <td>{data.continent}</td>
+                    <td>{data.region}</td>
+                    <td>{data.population}</td>
+                    <td>{(data.capital == null) ? "-" : data.capital.name}</td>
+                    </tr>
+                ))}
+            </tbody>
         </table>
     )
 }
