883d223f01616355b4f53515691c8d9567629adf
576125176251825152 <tomhadley27@icloud.com>
2022-04-25 00:31:21 +0100

    bingaboomabingo

--- src/main/java/com/SET08103/cw/api/Controller.java
+++ src/main/java/com/SET08103/cw/api/Controller.java
@@ -220,6 +220,16 @@ public class Controller {
                 return DataParser.toJson(cities.subList(0, topN));
             }
 
+            // https://github.com/violin-suzutsuki/GA-SE-CW/issues/43
+            // The top N populated capital cities in a continent where N is provided by the user.
+            case 43: {
+                List<City> cities = DataParser.getCapitalCitiesInContinent(input);
+                cities.sort(Comparator.comparing(City::getPopulation).reversed());
+
+                int topN = Integer.parseInt(input2);
+                return DataParser.toJson(cities.subList(0, topN));
+            }
+
             // https://github.com/violin-suzutsuki/GA-SE-CW/issues/45
             // The population of people, people living in cities, and people not living in cities in each continent
             case 45: {
