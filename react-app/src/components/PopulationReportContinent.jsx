db28f8eab5a898f642f002a6b108bd8752ce4617
Matthew Jack <matthew.jack@sds.co.uk>
2022-04-25 02:21:23 +0100

    added table for continent population

---
+++ react-app/src/components/PopulationReportContinent.jsx
@@ -0,0 +1,29 @@
+export default function PopulationReport(tableData) {
+    return (
+        <div className="overflow-x-auto">
+            <table className="table table-zebra w-full">
+                <thead>
+                    <tr>
+                        <th>Name (Continent)</th>
+                        <th>Total Population (Continent)</th>
+                        <th>Total Population Living In Cities (Continent)</th>
+                        <th>Total Population Living Not In Cities (Continent)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableData.map(data => {
+                        return(
+                            <tr>
+                                <td>{data.name}</td>
+                                <td>{data.totalPopulation}</td>
+                                <td>{data.populationInCities}</td>
+                                <td>{data.populationNotInCities}</td>
+                            </tr>
+                        )
+                    }
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
