660e2dac2888525afbedab1764523fbce70d4466
Matthew Jack <matthew.jack@sds.co.uk>
2022-04-25 22:48:05 +0100

    updated header file to include report names and category

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -22,6 +22,7 @@ export default function Header({ id }) {
                                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                                     <span className="block xl:inline">{header.reportName}</span>{' '}
                                     <span className="block text-indigo-600">ID - {header.id}</span>
+                                    <span className="block xl:inline">Category - {header.category}</span>
                                 </h1>
                                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl md:mt-5 md:text-xl lg:mx-0">{header.description}</p>
                             </div>
