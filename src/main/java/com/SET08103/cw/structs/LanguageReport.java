6f1ccfdcb7991d4d5dee7cbef70510c9c9885e76
violin-suzutsuki <93684340+violin-suzutsuki@users.noreply.github.com>
2022-04-26 10:34:38 +0100

    comments part 1

--- src/main/java/com/SET08103/cw/structs/LanguageReport.java
+++ src/main/java/com/SET08103/cw/structs/LanguageReport.java
@@ -9,23 +9,49 @@ public class LanguageReport {
     private String language, percentOfWorld;
     private long population;
 
+    /**
+     * intialize new instance of a language report
+     *
+     * @param language
+     * @param population
+     */
     public LanguageReport(String language, long population) {
         this.language = language;
         this.population = population;
     }
 
+    /**
+     * get language of languagereport
+     *
+     * @return language
+     */
     public String getLanguage() {
         return language;
     }
 
+    /**
+     * get population of languagereport
+     *
+     * @return population
+     */
     public long getPopulation() {
         return population;
     }
 
+    /**
+     * set percent of world string
+     *
+     * @param str
+     */
     public void setPercentOfWorld(String str) {
         percentOfWorld = str;
     }
 
+    /**
+     * get percent of world string
+     *
+     * @return percent of world
+     */
     public String getPercentOfWorld() {
         return percentOfWorld;
     }
